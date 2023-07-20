3b542505ba070a2305c5350b41d207dcd0e53271
kouete kenneth <kouete678@gmail.com>
2023-07-20 08:21:59 +0100

    Restaurant website: Temporary Front-End Time taken 24 hours

--- week-two-learn/index.js
+++ week-two-learn/index.js
@@ -3,15 +3,56 @@
 */
 
 $(document).ready(function(){
+    $('#DownArrow').on('click',function(){
+        let Thirty=[9,4,6,11];
+        let day = parseInt($('#day').val());
+       let month= parseInt($('#month').val());
+       let year=parseInt($('#year').val());
 
-    $(DownArrow).on('click',function(){
-        let day = $('#day').val();
-       let month= $('#month').val();
-       let year=$('#year').val();
+       // Handle Month Validity
+      /* if (isNaN(month)||isNaN(day)||isNaN(year)) {
+          alert('No field Should left Empty');
+          return;
+       }*/
+       if ((month<1||month>12)) {
+
+                $('#main-1>span:nth-of-type(2)>i').css({
+                    visibility:'visible'
+                });
+       }
+       else{
+                $('#main-1>span:nth-of-type(2)>i').css({
+                    visibility:'hidden'
+                });             
+       }
+
+       //Handle Day validity
+       if ((day<1||day>31)||(Thirty.includes(month)&&day>30)||(month===2&&day>29)) {
+            $('#main-1>span:nth-of-type(1)>i').css({
+                visibility:'visible'
+            });
+       }
+       else{
+            $('#main-1>span:nth-of-type(1)>i').css({
+                visibility:'hidden'
+            });        
+       }
+        //Handle Day validity
+       if (year>Date().substring(11,15)&&year<1) {
+            $('#main-1>span:nth-of-type(1)>i').css({
+                visibility:'visible'
+            });  
+       } else {
+            $('#main-1>span:nth-of-type(3)>i').css({
+                visibility:'hidden'
+            });
+            $('#Y1').val()='20';    
+       }
+      
         $('#main-3>ul>li>input').css({
             'background-color':'red',
-             width: '7vw',
-             height:'7vh'
+             width: '30px',
+             height:'25px'
         });
     });
 });
